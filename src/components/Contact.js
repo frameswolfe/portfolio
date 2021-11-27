eacfe01243a3df83a7e36aa1ffeb6301682dc9dc
James Wolfe <jr.wolfe@outlook.com>
2021-11-27 14:29:55 +0000

    font awesome

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,18 @@
 import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelopeSquare } from "@fortawesome/free-solid-svg-icons"
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 function Contact(props) {
   return(
     <div>
       <h1>Contact</h1>
       <p>Please feel free to contact me by email at jr.wolfe@outlook.com</p>
-      <div>
-        <i class="fas fa-envelope-square"></i>
-        <i class="fab fa-linkedin"></i>
-        <i class="fab fa-github-square"></i>
+      <div className="icons">
+        <FontAwesomeIcon icon={faEnvelopeSquare} />
+        <FontAwesomeIcon icon={faLinkedin} />
+        <FontAwesomeIcon icon={faGithubSquare} />
       </div>
     </div>
   )
