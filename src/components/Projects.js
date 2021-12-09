56b94d890546b820f8987511f4d9030369bc2369
James Wolfe <jr.wolfe@outlook.com>
2021-12-09 16:24:25 +0000

    fixed thumbnail img link

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,22 +12,18 @@ function Projects(props) {
         <YoutubeEmbed embedId="me5B9C6wmac" />
       </div>
       <div className="thumbnail-container">
-        <div className="project-image">
-          <img src={homepage} alt="theGreenRoom Thumbnail" />
-        </div>
         <Thumbnail
           url="http://www.enterthegreenroom.co.uk"
           title="theGreenRoom"
+          image={homepage}
           description="A platform that aims to reconnect live music venues directly with emerging artists - streamlining the booking process and removing third parties from the equation."
         />
       </div>
       <div className="thumbnail-container">
-        <div className="project-image">
-          <img src={placeholder} alt="Wolfe's Watch List Thumbnail" />
-        </div>
         <Thumbnail
-          url="http://wolfes-watch-list.herokuapp.com"
+          url="#" // http://wolfes-watch-list.herokuapp.com"
           title="Wolfe's Watch List"
+          image={placeholder}
           description="A simple project which allows you to create lists of all your favourite movies. All the movies were seeded from The Movie Database API."
         />
       </div>
--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -4,6 +4,9 @@ function Thumbnail(props) {
   return (
     <div className="project">
       <a href={props.url} target="_blank" rel="noopener noreferrer">
+        <div className="project-image" >
+          <img src={props.image} alt={props.title} />
+        </div>
         <h3 className="project-title">{props.title}</h3>
         <div className="project-description">{props.description}</div>
       </a>
